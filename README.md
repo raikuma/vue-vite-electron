16f94c5afc5aa4ca9e8474803d6bc9f8e3c89f60
Woongoh Cho <whdnddh@naver.com>
2023-03-29 02:00:00 +0900

    Boilerplate

---
+++ README.md
@@ -0,0 +1,16 @@
+# Vue 3 + Vite + Electron
+
+This template should help get you started developing electron app using electron-forge with Vue 3 in Vite.
+
+```
+yarn install
+yarn dev
+
+yarn build
+yarn start
+
+yarn electron:build
+yarn electron:package
+```
+
+Reference: https://dev.to/olyno/how-to-create-an-electron-application-with-vite-im
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div>
+    <p>Chrome: <span id="chrome-version"></span></p>
+    <p>Node: <span id="node-version"></span></p>
+    <p>Electron: <span id="electron-version"></span></p>
+  </div>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
